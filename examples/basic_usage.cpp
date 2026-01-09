71f8faaf49be9ed1176fcf05f218fd0fb473bdbd
Thomas Lee <tms.lee9@gmail.com>
2026-01-09 12:08:25 -0500

    updated example

--- examples/basic_usage.cpp
+++ examples/basic_usage.cpp
@@ -1,6 +1,9 @@
 #include "thread_pool/thread_pool.hpp"
+#include <iostream>
 
 int main() {
     tp::ThreadPool pool(4);
+    auto future = pool.submit([](int x) {return x*2;}, 21);
+    std::cout<<future.get()<<std::endl; //42;
     return 0;
 }
